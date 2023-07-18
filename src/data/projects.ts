8596ff76d9c344f512ffa6cf0a155a2cfcdc6673
Min <min.elliot48@gmail.com>
2023-07-18 17:40:55 +1200

    memes project edded

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -12,14 +12,19 @@ const projects: Project[] = [
     link: 'https://github.com/MaeWolff/dictionary-app',
   },
   {
-    title: 'RED PING',
-    techs: ['ReactJS', 'Python', 'Rust', 'TypeScript', 'API'],
-    link: 'https://green-field-0a20fe310.3.azurestaticapps.net/',
-  },
-  {
     title: 'Username Generator',
     techs: ['Handlebars', 'JS'],
     link: 'http://technical-difficulties-username-in-20-qs.devacademy.nz/usernames',
+  },
+  {
+    title: 'Min Memes',
+    techs: ['full-stack', 'ReactJS', 'TypeScript', 'API'],
+    link: 'http://min-memes.devacademy.nz/',
+  },
+  {
+    title: 'RED PING',
+    techs: ['ReactJS', 'Python', 'Rust', 'TypeScript', 'API'],
+    link: 'https://green-field-0a20fe310.3.azurestaticapps.net/',
   },
 ]
 
